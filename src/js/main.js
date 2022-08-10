7181cfdc19bb9021fa974e2df076b80c358be079
[4기-2반] 장소영 <soy5360@naver.com>
2022-08-11 04:22:46 +0900

    [Bug] skeleton에 texture를 입히기위한 고민

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-import * as THREE from './jsm/build/three.module.js'
-import {OrbitControls} from "./jsm/controls/OrbitControls.js";
-import {BVHLoader} from "./jsm/loaders/BVHLoader.js";
+import * as THREE from "./jsm/build/three.module.js";
+import { OrbitControls } from "./jsm/controls/OrbitControls.js";
+import { BVHLoader } from "./jsm/loaders/BVHLoader.js";
+import { GLTFLoader } from "./jsm/loaders/GLTFLoader.js";
 
 const clock = new THREE.Clock(); // Animation 동작을 위해서 구현해두었다.
 
@@ -11,129 +12,159 @@ let animeStatus = true;
 
 // -----------------------------------------------Three.js 의 기본 셋팅 ------------------------------------------
 /*
-*   기본은 Three.js에서 돌아가는 것이다. 그에 따라 몇가지 설정을 해주어야 한다.
-*   설정에 대한 부분은 한번에 처리하기 위해서 모듈화하여 init 함수에 담고자 했다.
-*
-*/
+ *   기본은 Three.js에서 돌아가는 것이다. 그에 따라 몇가지 설정을 해주어야 한다.
+ *   설정에 대한 부분은 한번에 처리하기 위해서 모듈화하여 init 함수에 담고자 했다.
+ *
+ */
 
 function initBvh() {
-    // 캔바스 설정
-    const canvas = document.querySelector("#three-canvas");
+  // 캔바스 설정
+  const canvas = document.querySelector("#three-canvas");
 
-    // Scene에 대한 설정
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( "#E1FFE1" ); // 배경색에 대한 설정이다.
+  // Scene에 대한 설정
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("#E1FFE1"); // 배경색에 대한 설정이다.
 
-    scene.add( new THREE.GridHelper( 800, 10 ) );
-    scene.add( new THREE.AxesHelper(600))
+  scene.add(new THREE.GridHelper(800, 10));
+  scene.add(new THREE.AxesHelper(600));
 
-    // 카메라
-    camera = new THREE.PerspectiveCamera(
-        60,
-        window.innerWidth / window.innerHeight,
-        1,
-        1000
-    );
-    camera.position.set( 0, 200, 300 );
-    scene.add(camera); // 카메라를 씬에 넣어주자.
+  // 카메라
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.set(0, 200, 300);
+  scene.add(camera); // 카메라를 씬에 넣어주자.
 
-    // renderer에 대한 설정
-    renderer = new THREE.WebGLRenderer( {
-        canvas : canvas,
-        antialias: true
-    } );
-    renderer.setPixelRatio( window.devicePixelRatio); // 차후 필요하면 삼항 연산자를 이용해서 정리해두자.
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    //document.body.appendChild( renderer.domElement );
+  // renderer에 대한 설정
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio); // 차후 필요하면 삼항 연산자를 이용해서 정리해두자.
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //document.body.appendChild( renderer.domElement );
 
-    controls = new OrbitControls( camera, renderer.domElement );
-    controls.minDistance = 300;
-    controls.maxDistance = 700;
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 300;
+  controls.maxDistance = 700;
 
-    window.addEventListener( 'resize', onWindowResize ); // 화면 사이즈가 변화하면 그에 맞춰서 변화해야 하는데 이에 대한 맞춤 설정이다.
+  window.addEventListener("resize", onWindowResize); // 화면 사이즈가 변화하면 그에 맞춰서 변화해야 하는데 이에 대한 맞춤 설정이다.
 
-    // gui.add(camera.position,'y',-1000,1000,0.01);
-
+  // gui.add(camera.position,'y',-1000,1000,0.01);
 }
 
 // 사이즈 조절과 관련된 함수이다.
 function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 // ------------------------------------------------BVH Loader --------------------------------------------------
 /*
-*   BVH Loader
-*   Reference : 참고 레퍼런스가 없어서 소스코드를 보아야 함.
-*
-*   현재 우리는 개발을 하는게 중요하므로, 동작원리에 대한 세부적인 이해보다는 아래와 같이 스켈레톤으로 구현할 수 있는 방식으로
-*   return 된다는 것을 확인하면 된다.
-*
-*
-*/
+ *   BVH Loader
+ *   Reference : 참고 레퍼런스가 없어서 소스코드를 보아야 함.
+ *
+ *   현재 우리는 개발을 하는게 중요하므로, 동작원리에 대한 세부적인 이해보다는 아래와 같이 스켈레톤으로 구현할 수 있는 방식으로
+ *   return 된다는 것을 확인하면 된다.
+ *
+ *
+ */
+// function loadBVH(strBVH) {
+//   const loader = new BVHLoader(); // BVH Loader를 파싱 받는다.
+//   // BVH Loader를 통해 받은 정보는 result로 나오게 되는데, 이를 바탕으로 skeleton을 이용해서 구현하게 된다.
+//   // 이 부분이 핵심으로, 여기서 구현된 loader가 해당 내용을 스켈레톤 정보로 뿌리게 됨.
+//   let result = loader.parse(strBVH);
+//   skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0]);
+//   skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to THREE.SkeletonHelper directly
+//   const boneContainer = new THREE.Group();
+//   boneContainer.add(result.skeleton.bones[0]);
+
+//   scene.add(skeletonHelper);
+//   scene.add(boneContainer);
+
+//   // play animation
+//   mixer = new THREE.AnimationMixer(skeletonHelper);
+//   mixer.clipAction(result.clip).setEffectiveWeight(1.0).play();
+// }
 function loadBVH(strBVH) {
-    const loader = new BVHLoader(); // BVH Loader를 파싱 받는다.
+  const loader = new BVHLoader();
+  // BVH Loader를 통해 받은 정보는 result로 나오게 되는데, 이를 바탕으로 skeleton을 이용해서 구현하게 된다.
+  // 이 부분이 핵심으로, 여기서 구현된 loader가 해당 내용을 스켈레톤 정보로 뿌리게 됨.
+  let result = loader.parse(strBVH);
+  console.log("리절트 : ", result);
+  skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0]);
+  console.log("스켈레톤 지오메트리 : ", skeletonHelper.geometry);
+  skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to THREE.SkeletonHelper directly
+  console.log("리절트 스켈레톤 : ", result.skeleton);
+  // result.skeleton.bones[0].boneTextureSize = 100;
+  // result.skeleton.bones[0].boneTexture = new THREE.MeshBasicMaterial({
+  //   color: 0xe80227,
+  //   blending: 100,
+  // });
+  const boneContainer = new THREE.Group();
+  boneContainer.add(result.skeleton.bones[0]);
+  console.log(boneContainer);
+  const mesh = new THREE.SkinnedMesh(
+    boneContainer.children[0].geometry,
+    new THREE.MeshStandardMaterial({ color: 0xe80227, skinning: true })
+  );
+  console.log(mesh);
+  scene.add(skeletonHelper);
+  scene.add(boneContainer);
 
-// BVH Loader를 통해 받은 정보는 result로 나오게 되는데, 이를 바탕으로 skeleton을 이용해서 구현하게 된다.
-// 이 부분이 핵심으로, 여기서 구현된 loader가 해당 내용을 스켈레톤 정보로 뿌리게 됨.
-    let result = loader.parse(strBVH);
-    skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0]);
-    skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to THREE.SkeletonHelper directly
-
-    const boneContainer = new THREE.Group();
-    boneContainer.add(result.skeleton.bones[0]);
-
-    scene.add(skeletonHelper);
-    scene.add(boneContainer);
-
-    // play animation
-    mixer = new THREE.AnimationMixer(skeletonHelper);
-    mixer.clipAction(result.clip).setEffectiveWeight(1.0).play();
+  // play animation
+  mixer = new THREE.AnimationMixer(skeletonHelper);
+  mixer.clipAction(result.clip).setEffectiveWeight(1.0).play();
 }
 // BVH String을 BVH 파일 양식으로 바꾸어주는 함수
 // 서버에서 받아온 BVH 데이터가 문자열 형태로 줄단위로 쪼개져서 오기 떄문에 다시 이어주는 작업을 해주는 함수.
-function concatStrBVH(data){ // strBVH == return value
-    let strBVH = '';
-    for (let i = 0; i<data.length; i++){
-        strBVH += data[i];
-        strBVH += '\n';
-    }
-    return strBVH;
+function concatStrBVH(data) {
+  // strBVH == return value
+  let strBVH = "";
+  for (let i = 0; i < data.length; i++) {
+    strBVH += data[i];
+    strBVH += "\n";
+  }
+  return strBVH;
 }
 
 // -----------------------------------------------Animation 구현-------------------------------------------------
 function animate() {
+  const delta = clock.getDelta();
 
-    const delta = clock.getDelta();
-
-    if(animeStatus == true) {
-        if ( mixer ) mixer.update( delta ); // Mixer 구동을 위한 구현부
-        renderer.render( scene, camera );
-        requestAnimationFrame(animate);
-    }
-    else if(animeStatus == false){
-        requestAnimationFrame(animate);
-    }
-
+  if (animeStatus == true) {
+    if (mixer) mixer.update(delta); // Mixer 구동을 위한 구현부
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  } else if (animeStatus == false) {
+    requestAnimationFrame(animate);
+  }
 }
 // animation 동작 제어 (pause, run)
-function togleAnimate(){
-    if(animeStatus == true) {
-        animeStatus = false;
-    }
-    else if(animeStatus == false){
-        animeStatus = true;
-    }
-}
-function playAnimate(){
-    animeStatus = true;
-}
-function stopAnimate(){
+function togleAnimate() {
+  if (animeStatus == true) {
     animeStatus = false;
+  } else if (animeStatus == false) {
+    animeStatus = true;
+  }
 }
-export{initBvh,loadBVH,animate, playAnimate, stopAnimate, togleAnimate, concatStrBVH}
+function playAnimate() {
+  animeStatus = true;
+}
+function stopAnimate() {
+  animeStatus = false;
+}
+export {
+  initBvh,
+  loadBVH,
+  animate,
+  playAnimate,
+  stopAnimate,
+  togleAnimate,
+  concatStrBVH,
+};
